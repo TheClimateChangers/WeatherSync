d710a64a2a98b60c46e4beda9d116ea9411e1c1f
Giselle-A04 <giselleavila004@gmail.com>
2025-03-31 20:56:30 -0700

    Test

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function NotFound() {
     return <div>
         <h1>404 Not Found</h1>
-        <p>The page you're looking for doesn't exist!</p>
+        <p>The page you're looking for doesn't exist! Nuar! </p>
     </div>
 }
 
