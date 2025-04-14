5cf19c5f1c706026648b95e030cc83fea3a67498
marhad400 <markhaddad03@gmail.com>
2025-04-14 15:57:01 -0700

    multi-day forecast

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import YelpActivitiesView
+from .views import YelpActivitiesView, WeatherDataView, WeatherForecastView
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
-    path("weather/", views.WeatherDataView.as_view(), name="weather-data"),
+    path("weather/", WeatherDataView.as_view(), name="weather-data"),
+    path("weather/forecast/", WeatherForecastView.as_view(), name="weather-forecast"),
     path("yelp-activities/", YelpActivitiesView.as_view(), name="yelp-activities"),
 ]
