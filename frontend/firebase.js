25616ce9f6c1f74e1e131022187f3abaa3d50d12
mrone <mroneill30@gmail.com>
2025-04-14 21:35:19 -0700

    firebase env

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCcEk7ql9xhCefCVgkKm4GhoQwnoybLg2M",
-  authDomain: "tripsync-80368.firebaseapp.com",
-  projectId: "tripsync-80368",
-  storageBucket: "tripsync-80368.firebasestorage.app",
-  messagingSenderId: "461682188392",
-  appId: "1:461682188392:web:da5c0e78a9b8d54d079bdd",
-  measurementId: "G-3G7216W79S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
